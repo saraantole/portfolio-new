3722c0bf74601bbc75ec53fbc998fd18420323ab
saraa34 <sara.antole@gmail.com>
2021-02-11 16:13:05 +0100

    build

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -13,8 +13,6 @@ const GlobalStyle = createGlobalStyle`
     html {
         width: 100%;
         height: 100%;
-        overflow: auto;
-        background-color: ${({ theme }) => theme.colors.background}!important;
     }
 
     body {
