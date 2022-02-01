889db157bbe8f2596fa43ba6ee79f0fa5863fd23
unknown <sara.antole@gmail.com>
2022-02-01 14:05:49 +0100

    fixing

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
